e781b49941b358fc62e2f010ef3060df882849aa
dsinghl <31862881+dsinghl@users.noreply.github.com>
2020-01-15 19:38:25 +0000

    Adding travel_time, felixibility and validation
    This is a large change to the main travel planner module.
    
    Added validation to constructors:
    	- All classes are now robust and will only take expected input or throw up an error.
    	- CSV files are checked for correct format by checking for number of columns per row
    
    Cleaned up logic in functions:
    	- Private methods added to make code more readable
    
    New functionality:
    	- Added the travel_time() functions and print_time_stats() functions to Journey class.

--- travelplanner/planner.py
+++ travelplanner/planner.py
@@ -5,7 +5,16 @@ import csv
 
 
 class Route:
-    def __init__(self, route_filename):
+
+    def __init__(self, route_filename, speed=10):
+        self.__check_route_csv_format(route_filename)
+
+        self.route = self.__read_route_csv(route_filename)
+        self.__check_valid_route()
+
+        self.speed = speed
+
+    def __read_route_csv(self, route_filename):
         route_from_csv = []
         with open(route_filename) as csv_file:
             read_csv = csv.reader(csv_file, delimiter=",")
@@ -15,7 +24,36 @@ class Route:
                 stop = row[2]
                 step = (x_position, y_position, stop)
                 route_from_csv.append(step)
-        self.route = route_from_csv
+        return route_from_csv
+
+    def __check_route_csv_format(self, route_filename):
+        commas_per_line = 2
+        with open(route_filename) as csv_file:
+            for line in csv_file:
+                if line.count(",") != commas_per_line:
+                    raise IndexError(
+                        f"Please check {route_filename} follows correct format"
+                    )
+
+    def __check_valid_route(self):
+        chain_code = self.generate_cc()[1]
+        for digit in chain_code:
+            if int(digit) % 2 != 0:
+                raise ValueError(
+                    "Invalid route. Bus can only travel horizontally or vertically per step."
+                )
+
+    def _stop_locations(self):
+        stop_list = [
+            (np.asarray(location), stop) for *location, stop in self.route if stop
+        ]
+        return stop_list
+
+    def __str__(self):
+        return f"Route from {self.route[0][:2]} to {self.route[-1][:2]} with speed {self.speed} minutes per step"
+    
+    def __repr__(self):
+        return f"route(start={self.route[0][:2]}, end={self.route[-1][:2]}, speed={self.speed})"
 
     def timetable(self):
         """Generates a timetable for a route as minutes from its first stop. """
@@ -24,7 +62,7 @@ class Route:
         for step in self.route:
             if step[2]:
                 stops[step[2]] = time
-            time += 10
+            time += self.speed
         return stops
 
     def plot_map(self):
@@ -42,7 +80,7 @@ class Route:
         plt.show()
 
     def generate_cc(self):
-        """
+        r"""
         Converts route information into a Freeman chain code
          3   2   1
           \  |  /
@@ -72,29 +110,31 @@ class Route:
 
 class Passenger:
 
-    id_list = [0]
+    __lastID = 1
 
     def __init__(self, *, start, end, speed):
+        self.__check_valid_passenger(start, end, speed)
+        self.start = start
+        self.end = end
+        self.speed = speed
+        self.id = Passenger.__lastID
+        Passenger.__lastID += 1
+
+    def __check_valid_passenger(self, start, end, speed):
         validstart = type(start) == tuple and len(start) == 2
         validend = type(end) == tuple and len(end) == 2
         validspeed = type(speed) == float or int
         valid_init = validstart and validend and validspeed
-        if valid_init:
-            self.start = start
-            self.end = end
-            self.speed = speed
-        else:
-            raise TypeError(
-                "Please supply tuple of 2 values for start and end, and a float or integer for speed"
+        if not valid_init:
+             raise TypeError(
+                "Please supply tuple of 2 values for start and end positions, and a float or integer for speed"
             )
-        self.id = self.id_list[-1] + 1
-        self.id_list.append(self.id)
 
     def __str__(self):
         return f"Start point : {self.start} \nEnd point: {self.end} \nSpeed : {self.speed} "
 
     def __repr__(self):
-        return f"PassengerId({self.id})"
+        return f"passenger{self.id}({self.start},{self.end},{self.speed})"
 
     def walk_time(self):
         endpoint = np.asarray(self.end)
@@ -105,62 +145,116 @@ class Passenger:
 
 
 class Journey:
-    def __init__(self, route, passengers):
+    def __init__(self, passengers, route):
+        self.__valid_arguments(passengers, route)
+        self.passengers = passengers
+        self.route = route
+        self.total_passengers = len(passengers)
+
+    def __valid_arguments(self, passengers, route):
         validroute = isinstance(route, Route)
         valid_passenger_list = all(
             isinstance(element, Passenger) for element in passengers
         )
         valid_init = validroute and valid_passenger_list
-        if valid_init:
-            self.passengers = passengers
-            self.route = route
-        else:
+        if not valid_init:
             raise TypeError(
-                "must use route and passenger objects to initialise journey"
+                "Only route and passenger objects are allowed to create journey."
             )
 
     def _route(self):
         return self.route.route
 
-    def passenger_trip(self, passenger):
-        stops = [step for step in self._route() if step[2]]  # calculate closer stops
-        ## to start
-        distances = [
-            (
-                math.sqrt(
-                    (x - passenger.start[0]) ** 2 + (y - passenger.start[1]) ** 2
-                ),
-                stop,
-            )
-            for x, y, stop in stops
-        ]
-        closer_start = min(distances)
-        print(distances)
-        ## to end
-        distances = [
-            (math.sqrt((x - passenger.end[0]) ** 2 + (y - passenger.end[1]) ** 2), stop)
-            for x, y, stop in stops
-        ]
-        closer_end = min(distances)
+    def __find_closest_stop(self, position):
+        stop_locations = self.route._stop_locations()
+        position = np.asarray(position)
 
-        stop_pos = {v[2]: i for i, v in enumerate(stops)}
-        start_pos = stop_pos[closer_start[1]]
-        end_pos = stop_pos[closer_end[1]]
-        if start_pos >= end_pos:
-            closer_start = closer_end
+        distances = []
+        for location, stop in stop_locations:
+            distance = np.linalg.norm(location - position)
+            distances.append((distance, stop))
+
+        closest_stop = min(distances)
+        closest_stops = [stop for stop in distances if stop[0] == closest_stop[0]]
+
+        if len(closest_stops) == 1:
+            return closest_stops[0]
+        return closest_stops
+
+    def __pick_stop(self, stops, bus_location):
+        valid_location = {"start", "end"}
+        if bus_location not in valid_location:
+            raise ValueError(f"Bus location must be one of {valid_location}")
+
+        bus_times = self.route.timetable()
+        stop_times = [(bus_times[stop[1]], (stop[0], stop[1])) for stop in stops]
+
+        if bus_location == "start":
+            start_stop = max(stop_times)
+            return start_stop[1]
+        elif bus_location == "end":
+            end_stop = min(stop_times)
+            return end_stop[1]
+
+    def passenger_trip(self, passenger):
+
+        closer_start = self.__find_closest_stop(passenger.start)
+        closer_end = self.__find_closest_stop(passenger.end)
+
+        if type(closer_start) == list:
+            closer_start = self.__pick_stop(closer_start, "start")
+        if type(closer_end) == list:
+            closer_end = self.__pick_stop(closer_end, "end")
+
         return (closer_start, closer_end)
 
-    def passenger_trip_time(self, passenger):
-        walk_distance_stops = self.passenger_trip(passenger)
+    def _passenger_trip_time(self, passenger):
+        (walk_dist_start, start_stop), (walk_dist_end, end_stop) = self.passenger_trip(
+            passenger
+        )
         bus_times = self.route.timetable()
-        bus_travel = (
-            bus_times[walk_distance_stops[1][1]] - bus_times[walk_distance_stops[0][1]]
-        )
+
+        bus_travel = bus_times[end_stop] - bus_times[start_stop]
         walk_travel = (
-            walk_distance_stops[0][0] * passenger.speed
-            + walk_distance_stops[1][0] * passenger.speed
+            walk_dist_start * passenger.speed + walk_dist_end * passenger.speed
         )
-        return bus_travel + walk_travel
+        bus_walk_travel = bus_travel + walk_travel
+
+        walking_only = passenger.walk_time()
+
+        same_start_end_stop = start_stop == end_stop
+        end_stop_before_start_stop = bus_times[end_stop] < bus_times[start_stop]
+        walking_faster_than_bus = walking_only < bus_walk_travel
+
+        walking = (
+            same_start_end_stop or end_stop_before_start_stop or walking_faster_than_bus
+        )
+
+        if walking:
+            travel_time = {"walk": walking_only, "bus": 0}
+        else:
+            travel_time = {"walk": walk_travel, "bus": bus_travel}
+
+        return travel_time
+
+    def travel_time(self, passenger_id):
+        for passenger in self.passengers:
+            if passenger_id == passenger.id:
+                return self._passenger_trip_time(passenger)
+        return ValueError(f"A passenger with Id={passenger_id} does not exist.")
+
+    def print_time_stats(self):
+        walk_total = 0
+        bus_total = 0
+        for passenger in self.passengers:
+            time = self.travel_time(passenger.id)
+            walk_total += time["walk"]
+            bus_total += time["bus"]
+        av_bus_time = bus_total / self.total_passengers
+        av_walk_time = walk_total / self.total_passengers
+
+        print(f"Average time on bus: {av_bus_time:.0f} min")
+        print(f"Average walking time: {av_walk_time:.0f} min")
 
     def plot_bus_load(self):
         stops = {step[2]: 0 for step in self._route() if step[2]}
@@ -222,15 +316,19 @@ def read_passengers(passenger_filename):
     return passengers
 
 
+def __check_passenger_csv_format():
+    commas_per_line = 4
+    with open(route_filename) as csv_file:
+        for line in csv_file:
+            if line.count(",") != commas_per_line:
+                raise IndexError(
+                    f"Please check {route_filename} follows correct format"
+                )
+
+
 if __name__ == "__main__":
-    test = Route("route.csv")
-    print(test.route)
-    passengers = [
-        Passenger(start=start, end=end, speed=speed)
-        for start, end, speed in read_passengers("passengers.csv")
-    ]
-    print(passengers)
-    j = Journey(test, passengers)
-    a = j._route()
-    j.map()
-    j.plot_bus_load()
+    john = Passenger(start=(0,2), end=(8,1), speed=15) 
+    mary = Passenger(start=(0,0), end=(6,2), speed=12) 
+    route = Route("route.csv")
+    journey = Journey([mary, john], route)
+    print(journey.travel_time(mary.id))
