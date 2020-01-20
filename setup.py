9293b9aebd6f75dbeec407635f4d97aed678b2c5
dsinghl <31862881+dsinghl@users.noreply.github.com>
2020-01-20 00:42:37 +0000

    Adding Licese
    Added MIT license
     - LICENSE.md added
     - setup.py ammended for license

--- setup.py
+++ setup.py
@@ -1,16 +1,12 @@
 from distutils.core import setup
 
 setup(
-    name='tplanner',
-    description='A travelplanner package to calulate bus journeys',
-    version='0.1',
-    install_requires=['matplotlib','numpy',],
-    packages=['travelplanner'],
-    license='Creative Commons Attribution-Noncommercial-Share Alike license',
-    long_description=open('README.md').read(),
-    entry_points={
-        "console_scripts": [
-            "bussimula=travelplanner.__main__:main",
-        ]
-    },
+    name="travelplanner",
+    description="A travelplanner package to calulate bus journeys",
+    version="0.1",
+    install_requires=["matplotlib", "numpy"],
+    packages=["travelplanner"],
+    license="OSI Approved :: MIT License",
+    long_description=open("README.md").read(),
+    entry_points={"console_scripts": ["bussimula=travelplanner.command:main"]},
 )
